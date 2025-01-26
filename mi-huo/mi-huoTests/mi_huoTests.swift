8845e32f753cc65c40660dfbd1853150bf85fd02
Daniella Zhou <daniella.zhou@ibm.com>
2025-01-26 16:51:07 +0100

    init ios app

---
+++ mi-huo/mi-huoTests/mi_huoTests.swift
@@ -0,0 +1,16 @@
+//
+//  mi_huoTests.swift
+//  mi-huoTests
+//
+//  Created by Daniella Zhou on 2025-01-26.
+//
+
+import Testing
+
+struct mi_huoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
