858a76a1523f74ceec61f2ea9d31bdd277ab4cc1
Daniella Zhou <daniella.zhou@ibm.com>
2025-01-27 14:49:40 +0100

    update theme

--- server/main.py
+++ server/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 
 @app.post("/generate_confusion/")
-async def generate_confusion(theme: str):
+async def generate_confusion(theme: str=""):
     client = InferenceClient(
         api_key=HUGGINGFACE_API_KEY
     )
@@ -32,7 +32,15 @@ async def generate_confusion(theme: str):
     messages = [
         {
         "role": "user",
-        "content": f"生成一个迷惑行为。迷惑行为的定义是普通行为 + 反向逻辑 + 奇怪目的。- 普通行为：一个日常生活中常见的行为，比如吃饭、睡觉、工作、学习、运动、娱乐等。 - 反向逻辑：与普通行为的预期不符或相反。例如：\n早晨用咖啡洗脸，宣称这是抗疲劳护肤新方式。\n在老板总结时，突然站起来背诵圆周率。\n边开会边用望远镜看同事。\n\n不要重复指令，不要解释，用中文回答，省略思考。\n以主题'{theme}'生成一个迷惑行为。迷惑行为："
+        "content": f"""生成一个迷惑行为。通常用来形容那些看起来匪夷所思、不合常理或者让人难以理解的行为或言论。它包含了一种调侃和娱乐的成分，常常被用来形容让人摸不着头脑的场景或人类奇怪的举动。
+例如：
+早晨用咖啡洗脸，宣称这是抗疲劳护肤新方式。
+在老板总结时，突然站起来背诵圆周率。
+边开会边用望远镜看同事。
+
+不要重复指令，不要解释，用中文回答，省略思考。
+
+生成一个迷惑行为。"""
                     f"随机编号: {random.randint(1, 100000)}"
     }
     ]
@@ -40,9 +48,9 @@ async def generate_confusion(theme: str):
     completion = client.chat.completions.create(
     model="google/gemma-2-9b-it", 
 	messages=messages, 
-	temperature=1.2,
+	temperature=1.5,
 	max_tokens=100,
-	top_p=0.8,
+	top_p=0.7,
     stop=["/n"]
 )
 
